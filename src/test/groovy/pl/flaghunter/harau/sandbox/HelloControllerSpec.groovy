b4a7891b799fee16d8fb428815706d0b8d64ab03
pmajcher <piotr.majcher.pl@gmail.com>
2016-11-11 22:56:08 +0100

    Add integration test

---
+++ src/test/groovy/pl/flaghunter/harau/sandbox/HelloControllerSpec.groovy
@@ -0,0 +1,26 @@
+package pl.flaghunter.harau.sandbox
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import spock.lang.Specification
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+
+@SpringBootTest(classes = Application, webEnvironment = RANDOM_PORT)
+class HelloControllerSpec extends Specification {
+
+	@Autowired
+	TestRestTemplate restTemplate
+
+	def "should return Fooooo for root path"() {
+		when:
+		ResponseEntity entity = this.restTemplate.getForEntity("/", String.class);
+
+		then:
+		entity.statusCode == HttpStatus.OK
+		entity.body == "Fooooo"
+	}
+}
